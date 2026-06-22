79a8d83dfbfe8059a9ffdcc26f58926dfee71df4
guilherme.anschau <anschau.gui@gmail.com>
2026-06-22 09:45:35 -0300

    feat: add search autores

--- src/main/java/com/example/libraryapi/controller/AutorController.java
+++ src/main/java/com/example/libraryapi/controller/AutorController.java
@@ -13,6 +13,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -72,6 +73,16 @@ public class AutorController {
     public ResponseEntity<List<AutorDTO>> pesquisar(
           @RequestParam(value = "nome", required = false) String nome,
           @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade())
+                ).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
 
     }
 }
